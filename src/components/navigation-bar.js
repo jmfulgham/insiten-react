399448a3eae4d774f981ba6b7f138a31a6fcda55
Mone't Fulgham <monetfulgham@gmail.com>
2018-10-24 20:44:20 -0400

    Add material app bar in navigation

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
 import './navigation-bar.css';
 import '../responsive.css';
+import Toolbar from '@material-ui/core/Toolbar';
 
 export default function NavigationBar(props) {
     const links = props.links.map((link, index) => (
@@ -13,13 +14,17 @@ export default function NavigationBar(props) {
     ));
 
     return (
-        <div className="row">
+        <div>
+        <AppBar position='static' style={[{flex: 'grow', color: '#FFFFFF'}]} color="primary">
+        <Toolbar>
             <img src={props.img} alt="Insiten React App" className="logo" />
             <nav role="navigation" className="col-12">
                 <ul>
                     {links}
                 </ul>
             </nav>
+            </Toolbar>
+            </AppBar>
         </div>
     );
 }
