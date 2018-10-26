1f0280a8d03eaf666b79ac058b7492e9ef7b1e7e
Mone't Fulgham <monetfulgham@gmail.com>
2018-10-26 10:21:20 -0400

    Remove <NavigationBar /> and add <HeaderBar />

---
+++ src/components/headerBar/HeaderBar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import './headerBar.css';
+import Toolbar from '@material-ui/core/Toolbar';
+import links from "../../links.json";
+
+export default class HeaderBar extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+    return (
+        <div>
+        <AppBar position='static' style={{flexGrow: 1}} color="default">
+        <Toolbar>
+            <img src={this.props.img} alt="Insiten React App" className="logo" />
+            </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavigationBar from './components/navigation-bar';
-import CreateForm from './components/create-form';
-import './index.css';
-import './responsive.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import HeaderBar from "./components/headerBar/HeaderBar";
+import CreateForm from "./components/create-form";
+import "./index.css";
+import "./responsive.css";
 
-const links = [{
-    text: 'Home',
-    href: '#'
-}, {
-    text: 'Portfolio',
-    href: '#'
-},
-              {
-    text: 'Financial Documentation',
-    href: '#'
-},
-              {
-    text: 'Public Disclosures',
-    href: '#'
-}];
-
-const img= 'http://www.insiten.com/wp-content/uploads/2017/12/logo_243_50.png'
-
-
+const img = "http://www.insiten.com/wp-content/uploads/2017/12/logo_243_50.png";
 ReactDOM.render(
   <div>
-  <NavigationBar title="Insiten" links={links} img={img}/>,
-  <CreateForm />
+    <HeaderBar title="Insiten" img={img} />,<CreateForm />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-registerServiceWorker();
