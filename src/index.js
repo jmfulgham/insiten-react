08c9252bbf29d0983adfcc1cf8e95cb06b760d38
Mone't Fulgham <monetfulgham@gmail.com>
2019-07-22 13:54:20 -0400

    Restructuring app in index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import HeaderBar from "./components/headerBar/HeaderBar";
 import CreateForm from "./components/createForm/CreateForm";
 import "./index.css";
 import "./responsive.css";
+import Company from "./components/company/Company";
 
 const img = "http://www.insiten.com/wp-content/uploads/2017/12/logo_243_50.png";
 ReactDOM.render(
-  <div>
-    <HeaderBar title="Insiten" img={img} />,
-    <CreateForm />
-  </div>,
-  document.getElementById("root")
+    <div>
+        <HeaderBar title="Insiten" img={img}/>,
+        <CreateForm/>
+        <div>
+            <Company/>
+        </div>
+    </div>,
+    document.getElementById("root")
 );
